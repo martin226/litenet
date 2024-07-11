22c31b92a9bc48618014b2c0fffd988cc7a03b57
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-07-11 18:37:57 -0400

    refactor: wrap in namespace

--- src/layers.cpp
+++ src/layers.cpp
@@ -5,112 +5,114 @@
 
 #include <stdexcept>
 
-litenet::Matrix litenet::layers::Layer::getWeights() const {
-    return weights;
-}
+namespace litenet::layers {
+    Matrix Layer::getWeights() const {
+        return weights;
+    }
 
-litenet::Matrix litenet::layers::Layer::getBiases() const {
-    return biases;
-}
+    Matrix Layer::getBiases() const {
+        return biases;
+    }
 
-void litenet::layers::Layer::setWeights(const Matrix &weights) {
-    this->weights = weights;
-}
+    void Layer::setWeights(const Matrix &weights) {
+        this->weights = weights;
+    }
 
-void litenet::layers::Layer::setBiases(const Matrix &biases) {
-    this->biases = biases;
-}
+    void Layer::setBiases(const Matrix &biases) {
+        this->biases = biases;
+    }
 
-litenet::layers::Dense::Dense(int inFeatures, int outFeatures, const std::string &activation, std::unique_ptr<litenet::initializers::Initializer> kernel_initializer, std::unique_ptr<litenet::initializers::Initializer> bias_initializer) {
-    this->inFeatures = inFeatures;
-    this->outFeatures = outFeatures;
-    this->activation = activation;
-    this->kernel_initializer = std::move(kernel_initializer);
-    this->bias_initializer = std::move(bias_initializer);
-}
+    Dense::Dense(int inFeatures, int outFeatures, const std::string &activation, std::unique_ptr<initializers::Initializer> kernel_initializer, std::unique_ptr<initializers::Initializer> bias_initializer) {
+        this->inFeatures = inFeatures;
+        this->outFeatures = outFeatures;
+        this->activation = activation;
+        this->kernel_initializer = std::move(kernel_initializer);
+        this->bias_initializer = std::move(bias_initializer);
+    }
 
-void litenet::layers::Dense::build() {
-    // for now, inputs is a matrix of shape (samples, features)
-    // weights is a matrix of shape (features, units)
-    // biases is a matrix of shape (units,)
-    weights = kernel_initializer->initialize(inFeatures, outFeatures);
-    biases = bias_initializer->initialize(outFeatures, 1);
-}
+    void Dense::build() {
+        // for now, inputs is a matrix of shape (samples, features)
+        // weights is a matrix of shape (features, units)
+        // biases is a matrix of shape (units,)
+        weights = kernel_initializer->initialize(inFeatures, outFeatures);
+        biases = bias_initializer->initialize(outFeatures, 1);
+    }
 
-litenet::Matrix litenet::layers::Dense::forward(const Matrix &inputs) {
-    // matrix multiplication:
-    // inputs: (samples, features)
-    // weights: (features, units)
-    // inputs * weights: (samples, units)
-    // biases: (units,)
-    // 
-    // z = inputs * weights + biases
-    this->inputs = inputs;
-    Matrix z = inputs * weights;
+    Matrix Dense::forward(const Matrix &inputs) {
+        // matrix multiplication:
+        // inputs: (samples, features)
+        // weights: (features, units)
+        // inputs * weights: (samples, units)
+        // biases: (units,)
+        // 
+        // z = inputs * weights + biases
+        this->inputs = inputs;
+        Matrix z = inputs * weights;
 
-    for (int i = 0; i < z.getRows(); i++) {
-        for (int j = 0; j < z.getCols(); j++) {
-            z(i, j) += biases(j, 0);
+        for (int i = 0; i < z.getRows(); i++) {
+            for (int j = 0; j < z.getCols(); j++) {
+                z(i, j) += biases(j, 0);
+            }
         }
+
+        return applyActivation(z);
     }
 
-    return applyActivation(z);
-}
+    std::tuple<Matrix, Matrix, Matrix> Dense::backward(const Matrix &dOutput) {
+        // Compute pre-activation
+        Matrix z = inputs * weights;
 
-std::tuple<litenet::Matrix, litenet::Matrix, litenet::Matrix> litenet::layers::Dense::backward(const Matrix &dOutput) {
-    // Compute pre-activation
-    Matrix z = inputs * weights;
-
-    // Add biases
-    for (int i = 0; i < z.getRows(); i++) {
-        for (int j = 0; j < z.getCols(); j++) {
-            z(i, j) += biases(j, 0);
+        // Add biases
+        for (int i = 0; i < z.getRows(); i++) {
+            for (int j = 0; j < z.getCols(); j++) {
+                z(i, j) += biases(j, 0);
+            }
         }
+
+        // Compute derivative of the activation function with respect to the pre-activation
+        Matrix dActivation = applyActivationPrime(z);
+        
+        // Compute delta as the Hadamard product of dOutput and dActivation
+        Matrix delta = dOutput.hadamard(dActivation);
+
+        // Compute gradients with respect to the weights and biases
+        Matrix dWeights = inputs.transpose() * delta;
+        Matrix dBiases = delta.sum(0).transpose(); // column-wise sum and then transpose to match the shape of biases
+
+        // Compute gradient with respect to the input
+        Matrix dInputs = delta * weights.transpose();
+
+        // Return dInputs, dWeights, and dBiases
+        return std::make_tuple(dInputs, dWeights, dBiases);
     }
 
-    // Compute derivative of the activation function with respect to the pre-activation
-    Matrix dActivation = applyActivationPrime(z);
-    
-    // Compute delta as the Hadamard product of dOutput and dActivation
-    Matrix delta = dOutput.hadamard(dActivation);
-
-    // Compute gradients with respect to the weights and biases
-    Matrix dWeights = inputs.transpose() * delta;
-    Matrix dBiases = delta.sum(0).transpose(); // column-wise sum and then transpose to match the shape of biases
-
-    // Compute gradient with respect to the input
-    Matrix dInputs = delta * weights.transpose();
-
-    // Return dInputs, dWeights, and dBiases
-    return std::make_tuple(dInputs, dWeights, dBiases);
-}
-
-litenet::Matrix litenet::layers::Dense::applyActivation(const Matrix &m) {
-    if (activation == "sigmoid") {
-        return activations::sigmoid(m);
-    } else if (activation == "relu") {
-        return activations::relu(m);
-    } else if (activation == "leakyRelu") {
-        return activations::leakyRelu(m);
-    } else if (activation == "tanh") {
-        return activations::tanh(m);
-    } else if (activation == "softmax") {
-        return activations::softmax(m);
+    Matrix Dense::applyActivation(const Matrix &m) {
+        if (activation == "sigmoid") {
+            return activations::sigmoid(m);
+        } else if (activation == "relu") {
+            return activations::relu(m);
+        } else if (activation == "leakyRelu") {
+            return activations::leakyRelu(m);
+        } else if (activation == "tanh") {
+            return activations::tanh(m);
+        } else if (activation == "softmax") {
+            return activations::softmax(m);
+        }
+        return m;
     }
-    return m;
-}
 
-litenet::Matrix litenet::layers::Dense::applyActivationPrime(const Matrix &m) {
-    if (activation == "sigmoid") {
-        return activations::sigmoidPrime(m);
-    } else if (activation == "relu") {
-        return activations::reluPrime(m);
-    } else if (activation == "leakyRelu") {
-        return activations::leakyReluPrime(m);
-    } else if (activation == "tanh") {
-        return activations::tanhPrime(m);
-    } else if (activation == "softmax") {
-        return activations::softmaxPrime(m);
+    Matrix Dense::applyActivationPrime(const Matrix &m) {
+        if (activation == "sigmoid") {
+            return activations::sigmoidPrime(m);
+        } else if (activation == "relu") {
+            return activations::reluPrime(m);
+        } else if (activation == "leakyRelu") {
+            return activations::leakyReluPrime(m);
+        } else if (activation == "tanh") {
+            return activations::tanhPrime(m);
+        } else if (activation == "softmax") {
+            return activations::softmaxPrime(m);
+        }
+        return m;
     }
-    return m;
 }
--- src/model.cpp
+++ src/model.cpp
@@ -7,101 +7,103 @@
 #include <numeric>
 #include <algorithm>
 
-litenet::Model::Model() : loss("mean_squared_error"), optimizer("sgd"), learningRate(0.001) {}
+namespace litenet {
+    Model::Model() : loss("mean_squared_error"), optimizer("sgd"), learningRate(0.001) {}
 
-void litenet::Model::add(std::unique_ptr<litenet::layers::Layer> layer) {
-    layers.push_back(std::move(layer));
-}
-
-void litenet::Model::compile(const std::string &loss, const std::string &optimizer, double learningRate) {
-    this->loss = loss;
-    this->optimizer = optimizer;
-    this->learningRate = learningRate;
-}
-
-void litenet::Model::fit(const Matrix &inputs, const Matrix &targets, int epochs, int batchSize) {
-    // Ensure parameters are valid
-    if (layers.empty()) {
-        throw std::runtime_error("Model is empty");
-    }
-    if (inputs.getRows() != targets.getRows()) {
-        throw std::invalid_argument("inputs and targets must have the same number of samples");
-    }
-    if (inputs.getRows() % batchSize != 0) {
-        throw std::invalid_argument("batch size must be a divisor of the number of samples");
+    void Model::add(std::unique_ptr<layers::Layer> layer) {
+        layers.push_back(std::move(layer));
     }
 
-    // Build the model
-    for (const auto &layer : layers) {
-        layer->build();
+    void Model::compile(const std::string &loss, const std::string &optimizer, double learningRate) {
+        this->loss = loss;
+        this->optimizer = optimizer;
+        this->learningRate = learningRate;
     }
 
-    // Train the model
-    for (int epoch = 0; epoch < epochs; epoch++) {
-            // TODO: implement batching
-            // TODO: implemen shuffling
+    void Model::fit(const Matrix &inputs, const Matrix &targets, int epochs, int batchSize) {
+        // Ensure parameters are valid
+        if (layers.empty()) {
+            throw std::runtime_error("Model is empty");
+        }
+        if (inputs.getRows() != targets.getRows()) {
+            throw std::invalid_argument("inputs and targets must have the same number of samples");
+        }
+        if (inputs.getRows() % batchSize != 0) {
+            throw std::invalid_argument("batch size must be a divisor of the number of samples");
+        }
 
-            Matrix batchInputs = inputs;
-            Matrix batchTargets = targets;
+        // Build the model
+        for (const auto &layer : layers) {
+            layer->build();
+        }
 
-            for (const auto &layer : layers) {
-                batchInputs = layer->forward(batchInputs);
-            }
+        // Train the model
+        for (int epoch = 0; epoch < epochs; epoch++) {
+                // TODO: implement batching
+                // TODO: implemen shuffling
 
-            Matrix dOutput;
-            if (loss == "mean_squared_error") {
-                dOutput = litenet::loss::meanSquaredErrorPrime(batchInputs, batchTargets);
-            } else if (loss == "mean_absolute_error") {
-                dOutput = litenet::loss::meanAbsoluteErrorPrime(batchInputs, batchTargets);
-            } else if (loss == "binary_crossentropy") {
-                dOutput = litenet::loss::binaryCrossentropyPrime(batchInputs, batchTargets);
-            } else if (loss == "categorical_crossentropy") {
-                dOutput = litenet::loss::categoricalCrossentropyPrime(batchInputs, batchTargets);
-            } else {
-                throw std::invalid_argument("unknown loss function");
-            }
+                Matrix batchInputs = inputs;
+                Matrix batchTargets = targets;
 
-            for (int j = layers.size() - 1; j >= 0; j--) {
-                std::tuple<Matrix, Matrix, Matrix> gradients = layers[j]->backward(dOutput); // dInput, dWeights, dBiases
-                dOutput = std::get<0>(gradients);
-                Matrix dWeights = std::get<1>(gradients);
-                Matrix dBiases = std::get<2>(gradients);
-
-                // TODO: implement optimizers
-                if (optimizer == "sgd") {
-                    layers[j]->setWeights(layers[j]->getWeights() - learningRate * dWeights);
-                    layers[j]->setBiases(layers[j]->getBiases() - learningRate * dBiases);
-                } else {
-                    throw std::invalid_argument("unknown optimizer");
+                for (const auto &layer : layers) {
+                    batchInputs = layer->forward(batchInputs);
                 }
-            }
 
-            // Calculate loss
-            double currentLoss;
-            if (loss == "mean_squared_error") {
-                currentLoss = litenet::loss::meanSquaredError(batchInputs, batchTargets);
-            } else if (loss == "mean_absolute_error") {
-                currentLoss = litenet::loss::meanAbsoluteError(batchInputs, batchTargets);
-            } else if (loss == "binary_crossentropy") {
-                currentLoss = litenet::loss::binaryCrossentropy(batchInputs, batchTargets);
-            } else if (loss == "categorical_crossentropy") {
-                currentLoss = litenet::loss::categoricalCrossentropy(batchInputs, batchTargets);
-            } else {
-                throw std::invalid_argument("unknown loss function");
-            }
-            
-            // TODO: implement metrics
-            std::cout << "Epoch " << (epoch + 1) << " | Loss: " << currentLoss << std::endl;
-    }
-}
+                Matrix dOutput;
+                if (loss == "mean_squared_error") {
+                    dOutput = litenet::loss::meanSquaredErrorPrime(batchInputs, batchTargets);
+                } else if (loss == "mean_absolute_error") {
+                    dOutput = litenet::loss::meanAbsoluteErrorPrime(batchInputs, batchTargets);
+                } else if (loss == "binary_crossentropy") {
+                    dOutput = litenet::loss::binaryCrossentropyPrime(batchInputs, batchTargets);
+                } else if (loss == "categorical_crossentropy") {
+                    dOutput = litenet::loss::categoricalCrossentropyPrime(batchInputs, batchTargets);
+                } else {
+                    throw std::invalid_argument("unknown loss function");
+                }
 
-litenet::Matrix litenet::Model::predict(const Matrix &inputs) {
-    if (layers.empty()) {
-        throw std::runtime_error("Model is empty");
+                for (int j = layers.size() - 1; j >= 0; j--) {
+                    std::tuple<Matrix, Matrix, Matrix> gradients = layers[j]->backward(dOutput); // dInput, dWeights, dBiases
+                    dOutput = std::get<0>(gradients);
+                    Matrix dWeights = std::get<1>(gradients);
+                    Matrix dBiases = std::get<2>(gradients);
+
+                    // TODO: implement optimizers
+                    if (optimizer == "sgd") {
+                        layers[j]->setWeights(layers[j]->getWeights() - learningRate * dWeights);
+                        layers[j]->setBiases(layers[j]->getBiases() - learningRate * dBiases);
+                    } else {
+                        throw std::invalid_argument("unknown optimizer");
+                    }
+                }
+
+                // Calculate loss
+                double currentLoss;
+                if (loss == "mean_squared_error") {
+                    currentLoss = litenet::loss::meanSquaredError(batchInputs, batchTargets);
+                } else if (loss == "mean_absolute_error") {
+                    currentLoss = litenet::loss::meanAbsoluteError(batchInputs, batchTargets);
+                } else if (loss == "binary_crossentropy") {
+                    currentLoss = litenet::loss::binaryCrossentropy(batchInputs, batchTargets);
+                } else if (loss == "categorical_crossentropy") {
+                    currentLoss = litenet::loss::categoricalCrossentropy(batchInputs, batchTargets);
+                } else {
+                    throw std::invalid_argument("unknown loss function");
+                }
+                
+                // TODO: implement metrics
+                std::cout << "Epoch " << (epoch + 1) << " | Loss: " << currentLoss << std::endl;
+        }
     }
-    Matrix predictions = inputs;
-    for (const auto &layer : layers) {
-        predictions = layer->forward(predictions);
+
+    Matrix Model::predict(const Matrix &inputs) {
+        if (layers.empty()) {
+            throw std::runtime_error("Model is empty");
+        }
+        Matrix predictions = inputs;
+        for (const auto &layer : layers) {
+            predictions = layer->forward(predictions);
+        }
+        return predictions;
     }
-    return predictions;
 }
